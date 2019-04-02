2d85a1d319f7720ebf9cda78f8c15c6b2d3b3eb2
Aleksandrs Krivickis <aleksandrs.krivikcis@gmail.com>
2019-04-02 21:40:13 +0800

    Automated push

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from distutils.core import setup
-import versioneer
 
 setup(
     name='SparkSafeDelta',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version='0.1.15',
     author='Aleksandrs Krivickis',
     author_email='aleksandrs.krivickis@gmail.com',
     packages=['sparksafedelta', 'sparksafedelta.tests'],
-    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
     url='http://pypi.python.org/pypi/SparkSafeDelta/',
     license='LICENSE.txt',
-    description='Tool that allows to automatically update schema of DataBricks Delta in case of Changes in data structure',
+    description='Combination of tools that allow more convenient use of PySpark within Azure DataBricks environment.',
     long_description=open('README.txt').read(),
     install_requires=[
-              'pyspark',
+                'numpy',
+                'pyspark',
           ],
 )
